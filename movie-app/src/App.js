de026f847c6a4421c13f83755ada4be720215062
Ezzeddine Jlassi <jlassiezzeddine@gmail.com>
2021-02-19 20:51:05 +0100

    start implementing header

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
-import Navigation from "./Components/Navigation/Navigation";
 import Content from "./Components/Content/Content";
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Navigation></Navigation>
       <Content></Content>
     </div>
   );
--- movie-app/src/Components/Header/Header.js
+++ movie-app/src/Components/Header/Header.js
@@ -6,7 +6,18 @@ class Header extends Component {
   render() {
     return (
       <div className="App-header">
-        <p>this is the header</p>
+        <nav>
+          <div className="logo">
+            <a href="#">Movies</a>
+          </div>
+          <div className="search--field">
+            <input type="text" name="search" id="search" />
+          </div>
+          <div className="nav--buttons">
+            <button className="filter--button">Filter</button>
+            <button className="add--movie">Add</button>
+          </div>
+        </nav>
       </div>
     );
   }
